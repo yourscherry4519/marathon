cfe5693214f90dcff3e7b43d45bf946fe30dea17
geekbrains <bulygina18@inbox.ru>
2021-06-19 12:36:15 +0500

    DGFHHJ

---
+++ Marathons/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Marathons
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Volunteer> volunteers = new List<Volunteer>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            FillTable();
+        }
+        
+        private void FillTable()
+        {
+            volunteers.Clear();
+            using (MarathonEntities1 db = new MarathonEntities1())
+            {
+                foreach (Volunteer u in db.Volunteer)
+                    volunteers.Add(u);
+                DataGridVolonter.ItemsSource = db.Volunteer.Local.ToBindingList();
+            }
+        }
+    }
+}
---
+++ Marathons/Win.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Marathons
+{
+    /// <summary>
+    /// Логика взаимодействия для Win.xaml
+    /// </summary>
+    public partial class Win : Window
+    {
+        public Win()
+        {
+            InitializeComponent();
+        }
+    }
+}
